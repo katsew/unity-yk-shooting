0a89b872db38a0ec9ad0d0a5d5a79f69a42adbea
System Administrator <root@yk-no-iMac.local>
2014-09-22 02:27:12 +0900

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Speed for the player
+	public float speed = 5;
+	
+	// Prefab of player bullet
+	public GameObject bullet;
+
+	// Shoot or not
+	private bool shouldShoot = true;
+
+	// Get Animator
+	protected Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Enter left/right
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		// Enter top/bottom
+		float y = Input.GetAxisRaw ("Vertical");
+
+		// Detect which direction player should move
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		rigidbody2D.velocity = direction * speed;
+
+
+	}
+
+	void initBullet () {
+
+		Vector3 playerPos = new Vector3(this.transform.position.x + 0.35f, this.transform.position.y + 0.8f, 0);
+		
+		// Instantiate player bullet
+		Instantiate(bullet, playerPos, this.transform.rotation);
+
+	}
+
+	void OnGUI () {
+		Event e = Event.current;
+
+		if (e.isKey && e.keyCode == KeyCode.J) {
+
+			if (animator.GetBool("shouldAnimate") == true) {
+				animator.SetBool("shouldAnimate", false);
+			}
+
+			if (shouldShoot == false) {
+				shouldShoot = true;
+				return;
+			} else {
+
+				animator.SetBool("shouldAnimate", true);
+				shouldShoot = false;
+				Invoke("initBullet", 0.45f);
+
+			}
+
+		}
+	}
+}
